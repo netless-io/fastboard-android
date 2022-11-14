2cddd57880b0c8ab2de3cef11a0bf812b60274e5
aderan <fenglibinjie@gmail.com>
2022-11-14 17:40:20 +0800

    Prepare for release nl_2.0.0-alpha.2

--- README.md
+++ README.md
@@ -32,7 +32,7 @@ android {
 }
 
 dependencies {
-    implementation "com.github.netless-io:fastboard-android:1.2.3"
+    implementation "com.github.netless-io:fastboard-android:nl_2.0.0-alpha.2"
 }
 ```
 
--- library/src/main/java/io/agora/board/fast/Fastboard.java
+++ library/src/main/java/io/agora/board/fast/Fastboard.java
@@ -11,7 +11,7 @@ import io.agora.board.fast.model.FastRoomOptions;
 import io.agora.board.fast.model.FastStyle;
 
 public class Fastboard {
-    public static final String VERSION = "1.2.3";
+    public static final String VERSION = "nl_2.0.0-alpha.2";
 
     private final FastboardView fastboardView;
     private FastStyle fastStyle;
