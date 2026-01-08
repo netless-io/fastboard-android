d4bfc6833d43c4d90e2242e18070161c6287a8fd
aderan <fenglibinjie@gmail.com>
2026-01-08 15:02:42 +0800

    update demo

--- app/src/main/java/io/agora/board/fast/sample/Constants.java
+++ app/src/main/java/io/agora/board/fast/sample/Constants.java
@@ -2,8 +2,8 @@ package io.agora.board.fast.sample;
 
 public class Constants {
     public static final String SAMPLE_APP_ID = "283/VGiScM9Wiw2HJg";
-    public static final String SAMPLE_ROOM_UUID = "785cc6d0e93511f0819537b901f4683c";
-    public static final String SAMPLE_ROOM_TOKEN = "NETLESSROOM_YWs9VWtNUk92M1JIN2I2Z284dCZleHBpcmVBdD0xNzY3NTk0MDMxMjY5Jm5vbmNlPTc4ODAyZDUwLWU5MzUtMTFmMC05NmE5LWFiMzg4NjE4OThhZiZyb2xlPTEmc2lnPWViMWU4NzY5ODY0NmUwYTZjMTY3YWNhMjFiMmRlZDA4NjI5NDRiNjg4MTdmYWY3MTNhN2MwMjI2YmY1ZjYzYzUmdXVpZD03ODVjYzZkMGU5MzUxMWYwODE5NTM3YjkwMWY0NjgzYw";
+    public static final String SAMPLE_ROOM_UUID = "cb40ade0ec5e11f0bb202dac1a31c602";
+    public static final String SAMPLE_ROOM_TOKEN = "NETLESSROOM_YWs9eTBJOWsxeC1IVVo4VGh0NyZub25jZT0xNzY3ODU1MjMzNDEyMDAmcm9sZT0wJnNpZz05MjE0ZGI5YjZkYWZlYTIyNThkNTkzZmI0NDI5MjFlODRkNTk5ZGY2NjdmNzRlMGE5M2Y5MzkzMmM2ZWNkOWU5JnV1aWQ9Y2I0MGFkZTBlYzVlMTFmMGJiMjAyZGFjMWEzMWM2MDI";
 
     public static final String KEY_ROOM_UUID = "room_uuid";
     public static final String KEY_ROOM_TOKEN = "room_token";
